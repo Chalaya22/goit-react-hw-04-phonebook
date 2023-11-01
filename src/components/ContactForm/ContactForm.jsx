ed55ed5169e841920fd7db35756fdcafa91c5ece
Chalaya22 <chalayaolga22@gmail.com>
2023-11-01 23:19:27 +0200

    comit 2

--- src/components/ContactForm/ContactForm.jsx
+++ src/components/ContactForm/ContactForm.jsx
@@ -1,62 +1,69 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import css from './ContactForm.module.css';
 
-class ContactForm extends Component {
-  state = {
-    name: ' ',
-    number: ' ',
-  };
+const ContactForm = ({ handleAddContact }) => {
+  const [name, setName] = useState(' ');
+  const [number, setNumber] = useState(' ');
 
-  handleInputChange = event => {
+  const handleInputChange = event => {
     const { name, value } = event.currentTarget;
-    this.setState({ [name]: value });
-    // this.setState({[event.currentTarget.name]: event.currentTarget.value})
+    // this.setState({ [name]: value });
+    switch (name) {
+      case 'name':
+        setName(value);
+        break;
+      case 'number':
+        setNumber(value);
+        break;
+
+      default:
+        return;
+    }
   };
-  reset = () => {
-    this.setState({ name: ' ', number: ' ' });
+  const reset = () => {
+    setName(' ');
+    setNumber(' ');
   };
 
-  handleSubmit = event => {
+  const handleSubmit = event => {
     event.preventDefault();
-    this.props.handleAddContact(this.state);
-    this.reset();
+    handleAddContact({ name, number });
+    reset();
   };
 
-  render() {
-    return (
-      <form onSubmit={this.handleSubmit} className={css.form}>
-        <label>
-          <p className={css.labelText}>Name</p>
-          <input
-            className={css.imputForm}
-            type="text"
-            name="name"
-            value={this.state.name}
-            onChange={this.handleInputChange}
-            placeholder="username: "
-            pattern="^[a-zA-Zа-яА-Я]+(([' \\-][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
-            required
-          />
-        </label>
-        <label>
-          <p className={css.labelText}>Number</p>
-          <input
-            className={css.imputForm}
-            type="tel"
-            name="number"
-            value={this.state.number}
-            onChange={this.handleInputChange}
-            placeholder="Enter number tel: "
-            pattern="\\+?\\d{1,4}?[ .\\-\\s]?\\(?\\d{1,3}?\\)?[ .\\-\\s]?\\d{1,4}[ .\\-\\s]?\\d{1,4}[ .\\-\\s]?\\d{1,9}"
-            required
-          />
-        </label>
+  return (
+    <form onSubmit={handleSubmit} className={css.form}>
+      <label>
+        <p className={css.labelText}>Name</p>
+        <input
+          className={css.imputForm}
+          type="text"
+          name="name"
+          value={name}
+          onChange={handleInputChange}
+          placeholder="username: "
+          pattern="^[a-zA-Zа-яА-Я]+(([' \\-][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+          required
+        />
+      </label>
+      <label>
+        <p className={css.labelText}>Number</p>
+        <input
+          className={css.imputForm}
+          type="tel"
+          name="number"
+          value={number}
+          onChange={handleInputChange}
+          placeholder="Enter number tel: "
+          pattern="\\+?\\d{1,4}?[ .\\-\\s]?\\(?\\d{1,3}?\\)?[ .\\-\\s]?\\d{1,4}[ .\\-\\s]?\\d{1,4}[ .\\-\\s]?\\d{1,9}"
+          required
+        />
+      </label>
 
-        <button className={css.buttonForm} type="submit">
-          Add contact
-        </button>
-      </form>
-    );
-  }
-}
+      <button className={css.buttonForm} type="submit">
+        Add contact
+      </button>
+    </form>
+  );
+};
 export default ContactForm;
